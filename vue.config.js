d5bb183075587a44897a42af71a2a9bdcfa9a104
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2023-12-07 23:50:19 +0800

    檔案同步

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production' ? '/fdp-gpt' : '/'
+  // transpileDependencies: true
 })
