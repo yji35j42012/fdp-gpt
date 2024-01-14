6aeb9daad78f4d077f2ebd0d84f56c3dd9ef496e
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2024-01-14 15:50:12 +0800

    檔案同步

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/fdp-gpt' : '/'
   // transpileDependencies: true
+
+  , chainWebpack: config => {
+    config.plugin('html')
+      .tap(args => {
+        args[0].title = "MED PILOT";
+        return args;
+      })
+  }
 })
